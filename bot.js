5784276cc405dddcd7a28c9afa9b009a581e67da
Nicxes <nicxesh@gmail.com>
2019-06-25 20:24:00 -0300

    Adding automatic static change status

--- bot.js
+++ bot.js
@@ -7,6 +7,7 @@ require('dotenv').config()
 const bot = new Client({
   token: process.env.DISCORD_TOKEN,
   prefix: process.env.DISCORD_PREFIX,
+  owner: process.env.DISCORD_OWNER,
   admins: [],
   selfbot: process.env.DISCORD_SELFBOT,
 
@@ -26,8 +27,31 @@ const logger = winston.createLogger({
   transports: [new winston.transports.Console()]
 })
 
+
+
+// When the bot is ready to use
 bot.on('ready', () => {
-  logger.log('info', 'Blackwatch is online')
+  // Message
+  logger.info(`Prefix: '${chalk.bgBlack.bold(bot.prefix)}'`)
+  logger.info(`${bot.user.username} is ready!`)
+
+  // Change status interval
+  bot.changeStatus = () => {
+    const statuses = [
+      { type: 0, name: 'emonika' },
+      { type: 0, name: 'eMonika' },
+      { type: 0, name: 'emOnika' },
+      { type: 0, name: 'emoNika' },
+      { type: 0, name: 'emonIka' },
+      { type: 0, name: 'emoniKa' },
+      { type: 0, name: 'emonikA' }
+    ]
+
+    const chooseStatus = statuses[~~(Math.random() * statuses.length)]
+    bot.editStatus({ name: chooseStatus.name, type: chooseStatus.type || 0 });
+    bot.logger.log(chalk(`Blackwatch status changed to: ${chalk.bgBlack.bold(chooseStatus.name)}`))
+  }
+  setInterval(() => bot.changeStatus(), 120000)
 })
 
 bot.run()
